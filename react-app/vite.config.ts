7844d60598c6f8ccfa4ce93efb3039df3fabbb13
Arvinjun <2429705898@qq.com>
2023-08-14 16:13:49 +0800

    feat: react app prod base config

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import qiankun from 'vite-plugin-qiankun';
+import path from 'path'
 import { name } from './package.json';
 
+const port = 8888; // dev port
+const useDevMode = true; // 是否使用开发模式
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    qiankun(name, {
-      useDevMode: true,
-    })
+    qiankun(name, { useDevMode })
   ],
   server: {
-    port: 8888,
+    fsServe: {
+      root: path.join(process.cwd(), '../../')
+    },
+    port,
     cors: true,
-  }
-})
+  },
+  // 生产模式下依旧不支持 publicPath, 需要将 vite.config.js 中 base 配置写死【致使多环境部署不便捷】
+  base: mode === 'development' ? '/' : `http://127.0.0.1:${port}/`,
+}))
